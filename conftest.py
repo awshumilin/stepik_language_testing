4fd00fb96853b8d09fb01bc4cfc31a304a919bf5
awshumilin <105726933+awshumilin@users.noreply.github.com>
2022-05-20 10:43:02 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -11,6 +11,6 @@ def browser(request):
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome('C:\chromedriver\chromedriver.exe', options=options)
+    browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
